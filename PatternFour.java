b09bd1ec03781405ee0572c4e4bdc93fbc9ce62c
Ganesh Maurya <gvgmaurya@gmail.com>
2020-08-20 14:50:17 +0530

    Pattern Four

---
+++ PatternFour.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class PatternFour {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k=1; k<=i; k++) {
+                System.out.print(k);
+            }
+            for (int l=i-1; l>=1; l--) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
