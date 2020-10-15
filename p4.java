89a2269af5b33fc078cf2dd36edf602ff8d0c92c
G V Ganesh Maurya <gvgmaurya@gmail.com>
2020-10-15 13:31:40 +0530

    exam

---
+++ p4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class p4 {
+    public static void main(String[] args) {
+        boolean ok = true;
+        switch(ok) {
+            case true: System.out.println("true");
+            break;
+            case false: System.out.println("false");
+            break;
+            default: System.out.println("default");
+            break;
+        }
+    }
+}
